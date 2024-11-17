7779a4f62465867d1cf321de9e3c21b93b9d83fb
Akash <akashshenvi93@gmail.com>
2024-11-17 17:14:45 +0530

    last update

--- Frontend/Travel_Planner/app/Home/Ai_Trip/SelectDate.jsx
+++ Frontend/Travel_Planner/app/Home/Ai_Trip/SelectDate.jsx
@@ -1,10 +1,68 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+import CalendarPicker from "react-native-calendar-picker";
 
-export default function SelectDate() {
+const TravelDates = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>SelectDate</Text>
+    <View style={styles.container}>
+      
+      <Text style={styles.headerText}>Travel Dates</Text>
+      
+      {/* Add content here */}
+      <View style={{
+        marginTop:30
+      }}>
+        <CalendarPicker
+         onDateChange={this.onDateChange} 
+         allowRangeSelection={true}
+         minDate={new Date()}
+         maxRangeDuration={30}
+         selectedRangeStyle={{
+            backgroundColor:'black'
+         }}
+         selectedDayTextColor={{
+            color:'white'
+         }}
+         />
+      </View>
+      <TouchableOpacity style={styles.continueButton} onPress={() => router.push('Home/Ai_Trip/SelectDate')}>
+        <Text style={styles.continueButtonText}>Continue</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  
+  headerText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    marginTop: 60,
+    fontFamily: 'Outfit', // Custom font you mentioned earlier
+    color: 'black',
+  },
+  continueButton: {
+    marginTop: 20,
+    paddingVertical: 15,
+    backgroundColor: '#000',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  continueButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default TravelDates;
