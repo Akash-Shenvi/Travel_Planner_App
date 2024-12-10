835c378ab7f13775a907795a7641de1c98f74b7e
Akash <akashshenvi93@gmail.com>
2024-12-10 16:22:55 +0530

    app almost completed

--- Backend/Backend_Code/Authentication.py
+++ Backend/Backend_Code/Authentication.py
@@ -10,12 +10,8 @@ import logging
 import os
 import google.generativeai as genai
 from flask_session import Session
-
 from flask import Flask, request, jsonify
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'base_data.json'), encoding='utf-8') as fobj:
-    api_key = json.load(fobj)['apikey']
 
-# Initialize Flask app
 app = Flask(__name__)
 
 api_key =api_key['api'] 
